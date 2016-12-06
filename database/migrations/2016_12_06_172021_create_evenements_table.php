c4018a67636f0152468bf966f5eabdb522ff7b63
Amandine <amandine.lesaint@yahoo.com>
2016-12-06 19:28:02 +0100

    Schema de a table evenements

---
+++ database/migrations/2016_12_06_172021_create_evenements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvenementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evenements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->longText('description');
+            $table->timestamp('date_debut');
+            $table->timestamp('date_fin');
+            $table->string('lieu');
+            $table->string('tarif');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('evenements');
+    }
+}
