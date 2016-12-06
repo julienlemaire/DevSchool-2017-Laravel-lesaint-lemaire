be01d50ac72ff0e835a8daf6eba308fa73da8726
julienlemaire <julien.lemaire@devinci.fr>
2016-12-06 13:02:51 +0100

    correction des routes et bdd admin

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
@@ -15,6 +16,9 @@ class IsAdmin
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if(Auth::check() && Auth::user()->isAdmin) {
+            return $next($request);
+        }
+        return redirect()->route('article.index');
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,17 +15,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
-});*/
+});
+
+Route::get('/home', function () {
+    return view('welcome');
+});
 
 
 Route::resource('/article', 'ArticleController');
+
+
+Route::auth();
+
 Route::get('/', 'HomeController@index');
-
-/*Route::auth();
-
-Route::get('/', 'HomeController@index');*/
 
 
 
