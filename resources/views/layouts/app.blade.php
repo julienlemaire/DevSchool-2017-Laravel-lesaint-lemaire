993531270a1093641ba7d6c49c7660094cb2c84b
julienlemaire <julien.lemaire@devinci.fr>
2016-12-07 22:30:43 +0100

    cacher admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,9 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}"><i class="fa fa-user"></i> Profil</a></li>
-                    <li><a href="{{ url('/admin') }}">Administrateur</a></li>
+                    @if(Auth::check() && Auth::user()->isAdmin)
+                        <li><a href="{{ url('/admin') }}">Administrateur</a></li>
+                    @endif
                 </ul>
 
                 <!-- Right Side Of Navbar -->
